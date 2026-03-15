430c747b1b098ed574774932325820357e825033
Itamar <itamar@Ariels-MacBook-Air.local>
2026-03-15 15:31:18 +0200

    Update Search Page

--- Project/Data/Search.cs
+++ Project/Data/Search.cs
@@ -5,7 +5,5 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
-        public string TeamName { get; set; }
-        public string LogoUrl { get; set; }
     }
 }
--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44db6550b70768dc8d4970781b04b704e169437e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c959f1fbd0d702287f9d8f06d5f415f23464863")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
